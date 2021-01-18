2c71fe3fa04ad6ab27015ea37dbeae64fb4aaafa
BrynL-F <hleonard@ualberta.ca>
2021-01-18 11:03:16 -0700

    Changing path to css

--- gulpfile.js
+++ gulpfile.js
@@ -32,20 +32,20 @@ gulp.task('browser-sync', ['sass', 'img', 'jekyll-build'], function() {
 
 // Compile files
 gulp.task('sass', function () {
-    return gulp.src('assets/css/scss/main.scss')
+    return gulp.src('./assets/css/scss/main.scss')
         .pipe(sass({
             outputStyle: 'expanded',
             onError: browserSync.notify
         }))
         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('assets/css'))
+        .pipe(gulp.dest('./assets/css'))
         .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('assets/css'));
+        .pipe(gulp.dest('./assets/css'));
 });
 
 // Compression images
 gulp.task('img', function() {
-	return gulp.src('assets/img/**/*')
+	return gulp.src('./assets/img/**/*')
 		.pipe(cache(imagemin({
 			interlaced: true,
 			progressive: true,
@@ -58,9 +58,9 @@ gulp.task('img', function() {
 
 // Watch scss, html, img files
 gulp.task('watch', function () {
-    gulp.watch('assets/css/scss/**/*.scss', ['sass']);
-    gulp.watch('assets/js/**/*.js', ['jekyll-rebuild']);
-    gulp.watch('assets/img/**/*', ['img']);
+    gulp.watch('./assets/css/scss/**/*.scss', ['sass']);
+    gulp.watch('./assets/js/**/*.js', ['jekyll-rebuild']);
+    gulp.watch('./assets/img/**/*', ['img']);
     gulp.watch(['*.html', '_layouts/*.html', '_includes/*.html', '_pages/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
